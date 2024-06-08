155bae29869e7c0c0505eb94b81bc40ec3175a5e
isaoasis <164797626+isaoasis@users.noreply.github.com>
2024-06-07 22:07:34 -0300

    connect database

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
 
     #Configuración de base dedatos 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:xOwTUgFKvBLbdUPFVcaEiWPWiKhfMHEr@roundhouse.proxy.rlwy.net:20405/railway"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://if0_36693380:6mAXMWKTwdumAOk@sql209.infinityfree.com:3306/if0_36693380_blog_db"
     
 class ProductionConfig(Config):
     DEBUG = False
